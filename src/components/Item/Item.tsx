78294983e58bde65f0dbc8b5e104a28639d746cd
Deaponn <62819066+Deaponn@users.noreply.github.com>
2022-06-27 17:42:09 +0200

    UI/UX changes
    Added info when there are no items with filtered Id

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -2,17 +2,29 @@ import { TableCell, TableRow } from "@mui/material";
 import { ItemAPI } from "../List/listAPI";
 import styled from "styled-components";
 
-interface ItemProps {
-    data: ItemAPI;
-}
+type ItemProps =
+    | {
+          data: ItemAPI;
+          noData?: never;
+      }
+    | {
+          data?: never;
+          noData: true;
+      };
 
 const StyledTableRow = styled(TableRow)<{ background: string }>`
     background-color: ${(props) => props.background};
 `;
 
-export default function Item({ data }: ItemProps) {
-    const { id, color, year, name } = data;
+export default function Item({ data, noData }: ItemProps) {
+    if (noData)
+        return (
+            <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                <TableCell>No item with specified Id exists</TableCell>
+            </TableRow>
+        );
 
+    const { id, color, year, name } = data;
     return (
         <StyledTableRow background={color} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
             <TableCell component="th" scope="row">
--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -17,6 +17,8 @@ export default function View() {
         }
     }, [fetchStatus, dispatch]);
 
+    const filteredList = filterValue !== "" ? itemList.filter((item) => item.id === parseInt(filterValue)) : itemList
+
     return (
         <TableContainer component={Paper}>
             <Table sx={{ minWidth: 350 }} aria-label="simple table">
@@ -29,9 +31,10 @@ export default function View() {
                 </TableHead>
                 <TableBody>
                     {
-                    itemList.length !== 0 && filterValue !== ""
-                        ? itemList.filter((item) => item.id === parseInt(filterValue)).map((item) => <Item key={item.id} data={item} />)
-                        : itemList.map((item) => <Item key={item.id} data={item} />)}
+                    filteredList.length === 0
+                        ? <Item noData />
+                        : filteredList.map((item) => <Item key={item.id} data={item} />)
+                        }
                 </TableBody>
             </Table>
             {itemList.length === 0 && [0, 1, 2, 3, 4].map((number) => <Skeleton key={number} variant="rectangular" animation="wave" height={53} />)}
